7d7240167ac8578f25aab4d6b2bf919cafe57c5d
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-08-23 17:58:57 -0300

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <h1>Hello Joshua</h1>;
+  const [techs, setTechs] = useState([]);
+  const [tech, setTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, tech]);
+    setTech('');
+  }
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input value={tech} type="text" onChange={e => setTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
